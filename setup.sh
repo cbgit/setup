e3d511aa6eeef6a1b7265f0e8dbc312cae5da192
carlbernard <carlbernard@cb-virtual-ubuntu.(none)>
2014-05-13 12:53:38 -0400

    Initial Commit

---
+++ setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
+# for headless setup. 
+
+# Install nvm: node-version manager
+# https://github.com/creationix/nvm
+sudo apt-get install -y git
+sudo apt-get install -y curl
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+# Load nvm and install latest production node
+source $HOME/.nvm/nvm.sh
+nvm install v0.10.26
+nvm use v0.10.26
+
+#installing tree
+sudo apt-get install tree -y
+
+
+# Install jshint to allow checking of JS code within emacs
+# http://jshint.com/
+npm install -g jlint
+
+# Install rlwrap to provide libreadline features with node
+# See: http://nodejs.org/api/repl.html#repl_repl
+sudo apt-get install -y rlwrap
+
+
+###Setting up Vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+curl -Sso ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+##Adding Vim bundle for JSLint
+cd ~/.vim/bundle
+git clone https://github.com/hallettj/jslint.vim.git
+cd ~
+
+# Install Heroku toolbelt
+# https://toolbelt.heroku.com/debian
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+# git pull and install dotfiles as well
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+
+git clone https://github.com/cbgit/dotfiles.git
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
